3da06edb31c8b82eaa35f1c656727dc8f8849730
sduzyn <2319052129@qq.com>
2025-05-31 18:26:50 +0800

    user manage and some detail

--- L-book-client/src/api/index.js
+++ L-book-client/src/api/index.js
@@ -10,7 +10,7 @@ export const reqLogin = ({account, password}) => ajax(BASE_URL+'/login', {accoun
 export const reqSignup = ({account, name, password, mail}) => ajax(BASE_URL+'/signup', {account, name, password,mail}, 'GET')
 
 // 3、改变用户状态
-export const reqChangeUser = (account) => ajax(BASE_URL+'/user/change', {account}, 'GET')
+export const reqChangeUser = (account) => ajax(BASE_URL+'/userchange', {account}, 'GET')
 
 // 4、获取所有用户状态
 export const reqGetUserState = () => ajax(BASE_URL+'/userstates')
